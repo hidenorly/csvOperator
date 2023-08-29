ce98c23fc6ae49cd5a57552131424e55c40f9214
hidenorly <twitte.harold@gmail.com>
2023-08-30 00:17:29 +0900

    Add OP_ADD

--- csvOperator.rb
+++ csvOperator.rb
@@ -12,9 +12,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 require 'fileutils'
 require 'optparse'
 require 'csv'
+
+OPERATION_NONE = 0
+OPERATION_UNION = 1
+OPERATION_AND = 2
+
 
 def outputCSV(result, path, verbose)
 	if path != nil then
@@ -45,14 +51,14 @@ def loadCSV(path)
 		File.open(path) do |file|
 			file.each_line do |aLine|
 				if( aLine.valid_encoding? ) then
-				    row = aLine.split(",")
-				    key = row[0].strip
-				    tmp = row.drop(1)
-				    vals=[]
-				    tmp.each do |a|
-				    	vals << a.to_i
-				    end
-				    data << {:key=>key.to_s, :vals=>vals.to_a}
+					row = aLine.split(",")
+					key = row[0].strip
+					tmp = row.drop(1)
+					vals=[]
+					tmp.each do |a|
+						vals << a.strip #.to_i
+					end
+					data << {:key=>key.to_s, :vals=>vals.to_a}
 				end
 			end
 		end
@@ -63,6 +69,7 @@ end
 def getTargetData(dataSet, key)
 	result=[]
 	dataSet.each do |file, dataArray|
+		# keyedData = dataArray.find { |aData| aData[:key] == key }
 		dataArray.each do |aData|
 			if aData[:key] == key then
 				aData[:vals].each do |aVal|
@@ -74,6 +81,8 @@ def getTargetData(dataSet, key)
 	return result
 end
 
+# 最初の要素から、次以降の要素に対してSUM/SUBする
+# =data[0]-sum(data[1]...data[N]), =data[0]+sum(data[1]...data[N])
 def operateData(data, operations)
 	result = []
 	operations.each do |anOperation|
@@ -97,23 +106,57 @@ def operateData(data, operations)
 	return result.to_a
 end
 
-def getUnionedKey(dataSet)
+
+def getOperatedKeys(dataSet, operationMode)
 	keys = []
 	dataSet.each do |file, dataArray|
+		tmpKeys = []
 		dataArray.each do |aData|
 			if( aData.has_key?(:key) ) then
 				key = aData[:key]
-				if !keys.include?( key ) then
-					keys << key
+				if !tmpKeys.include?( key ) then
+					tmpKeys << key
 				end
 			end
 		end
+		keys << tmpKeys if tmpKeys.length>0
 	end
 
-	return keys
+	result = keys[0]
+	for i in 1..(keys.length-1) do
+		case operationMode
+			when OPERATION_UNION then
+				result = result | keys[i]
+			when OPERATION_AND then
+				result = result & keys[i]
+		end
+	end
+
+	return result
 end
 
-def ensureKeys(dataSet, keys)
+# --- for and ------
+def ensureDatasetWithKeysForAnd(dataSet, keys)
+	dataSet.each do |file, dataArray|
+		newDataArray = []
+		dataArray.each do |aData|
+			if keys.include?(aData[:key]) then
+				newDataArray << aData
+			end
+		end
+		dataSet[file] = newDataArray
+	end
+
+	return dataSet
+end
+
+
+# --- for union ------
+# dataSetは[csvFileName] = dataの配列 (:key(string), :vals(array))になっている
+# keysは、全dataSetのkeyの和集合
+# dataSetのdataの配列の中を探索して、keyに漏れがあった場合は、dataSet[そのcsvFilename]に、
+# :key=>そのkey, :vals=>0...のarrayを追加することで各dataSet[csvFilename]のkeyがensureされるようにする
+def ensureDatasetWithKeysForUnion(dataSet, keys)
 	dataSet.each do |file, dataArray|
 		missingKeys = []
 		dataNumMax = 0
@@ -144,6 +187,41 @@ def ensureKeys(dataSet, keys)
 	return dataSet
 end
 
+def addOriginalDataToResult(dataSet, result)
+	dataSet.each do |file, dataArray|
+		dataArray.each do |aData|
+			if( aData.has_key?(:key) ) then
+				key = aData[:key].to_s
+				vals = aData[:vals].to_a
+
+				if !result.has_key?( key ) then
+					result[ key ] = vals
+				else
+					result[ key ] = result[ key ] + vals  # array + array (= unioned array)
+				end
+			end
+		end
+	end
+end
+
+# --- utility
+def removeAllZero(result)
+	result2 = {}
+	result.each do |key, vals|
+		flag = false
+		vals.each do |aVal|
+			if aVal.to_i!=0 || aVal.to_f!=0.0 then
+				flag=true
+				break
+			end
+		end
+		if flag then
+			result2[key] = vals
+		end
+	end
+	return result2
+end
+
 
 #---- main --------------------------
 options = {
@@ -155,7 +233,7 @@ options = {
 }
 
 opt_parser = OptionParser.new do |opts|
-	opts.banner = "Usage: --source=dataset1.csv;dataset2.csv --format=[union],[delta|sum] ... (options)"
+	opts.banner = "Usage: --source=dataset1.csv;dataset2.csv --format=[and|union],[delta|sum] ... (options)"
 
 	opts.on("-s", "--source=", "Specify source files (data1.csv,data2.csv)") do |source|
 		options[:source] = source
@@ -193,28 +271,38 @@ files.each do |aFile|
 	dataSet[aFile] = loadCSV(aFile)
 end
 
-# ensure dataSey with unioned keys
-keys = getUnionedKey(dataSet)
-dataSet = ensureKeys(dataSet, keys)
-
-# for union (print inputed CSV datas)
+# result
 result = {}
-dataSet.each do |file, dataArray|
-	dataArray.each do |aData|
-		if( aData.has_key?(:key) ) then
-			key = aData[:key].to_s
-			vals = aData[:vals].to_a
 
-			if options[:format].include?("union") then
-				if !result.has_key?( key ) then
-					result[ key ] = vals
-				else
-					result[ key ] = result[ key ] + vals
-				end
-			end
-		end
-	end
+# check mode
+operationMode = OPERATION_UNION
+enableOutputOriginalData = false
+if options[:format].include?("union") then
+	operationMode = OPERATION_UNION
+	enableOutputOriginalData = !options[:format].include?("(union)")
+elsif options[:format].include?("and") then
+	operationMode = OPERATION_AND
+	enableOutputOriginalData = !options[:format].include?("(and)")
 end
+
+# get unioned/and-ed keys
+keys = getOperatedKeys(dataSet, operationMode)
+
+
+# filter dataSet with unioned/and-ed keys
+case operationMode
+	when OPERATION_UNION then
+		# ensure dataSey with unioned keys
+		dataSet = ensureDatasetWithKeysForUnion(dataSet, keys)
+
+	when OPERATION_AND then
+		# ensure dataSey with and-ed keys
+		dataSet = ensureDatasetWithKeysForAnd(dataSet, keys)
+end
+
+# store data to print inputed CSV datas
+addOriginalDataToResult(dataSet, result) if enableOutputOriginalData
+
 
 # for operation
 operations = options[:format].split(",")
@@ -225,21 +313,9 @@ if operations then
 	end
 end
 
+
 # remove all zero
-result2 = {}
-result.each do |key, vals|
-	flag = false
-	vals.each do |aVal|
-		if aVal.to_i!=0 then
-			flag=true
-			break
-		end
-	end
-	if flag then
-		result2[key] = vals
-	end
-end
-result = result2
+result = removeAllZero(result)
 
 
 # sort TODO: use n-th data in vx
